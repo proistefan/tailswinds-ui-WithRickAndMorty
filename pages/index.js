9bd773e8d5c087326cae486b1f44bfd338eedce5
proistefan <i.stefan.pm@gmail.com>
2020-05-10 10:21:47 -0400

    Feature: Trying out auto commit

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function IndexPage() {
   return (
     <div class=" bg-gray-200 h-screen w-screen">
       <div class="shadow-lg hero bg-blue-200">
-        <h1 class="title">Learning Apollo with Next.js</h1>
+        <h1 class="title">Apollo with Next.js</h1>
       </div>
       <div class="flex justify-center">
         <Card link={'/page1'} heading={'Card Heading'} text={'Click Me to head on over to page 1'}/>
