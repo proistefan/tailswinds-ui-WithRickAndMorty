1c3ceb906365dfdac98de4ab4b43a21ab0b70933
proistefan <i.stefan.pm@gmail.com>
2020-05-14 11:09:19 -0400

    Feature: Playing with styling

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7e7eb6dd639701e29c8b";
+/******/ 	var hotCurrentHash = "d322b59aeb1497fefaa8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
@@ -853,7 +853,19 @@
 /******/ 		// Execute the module function
 /******/ 		var threw = true;
 /******/ 		try {
+/******/
+/******/ 		        var hasRefresh = typeof self !== "undefined" && !!self.$RefreshInterceptModuleExecution$;
+/******/ 		        var cleanup = hasRefresh
+/******/ 		          ? self.$RefreshInterceptModuleExecution$(moduleId)
+/******/ 		          : function() {};
+/******/ 		        try {
+/******/ 		        
 /******/ 			modules[moduleId].call(module.exports, module, module.exports, hotCreateRequire(moduleId));
+/******/
+/******/ 		        } finally {
+/******/ 		          cleanup();
+/******/ 		        }
+/******/ 		        
 /******/ 			threw = false;
 /******/ 		} finally {
 /******/ 			if(threw) delete installedModules[moduleId];
@@ -999,4 +1011,3 @@
 /******/ })
 /************************************************************************/
 /******/ ([]);
-//# sourceMappingURL=webpack.js.map
